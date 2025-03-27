8ad8202326af9c0fbadc381eb474a8010ed8d752
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-03-26 21:58:37 -0300

    Input para buscar pokemon funcional

--- src/App.js
+++ src/App.js
@@ -2,22 +2,44 @@ import React, {useEffect, useState} from "react";
 import './style.css';
 
 function App() {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState(null);
+  const [input, setInput] = useState('ditto');
+  const [loading, setLoading] = useState(false);
 
-  function loadAPI() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+  function loadAPI(pokemonNome) {
+    setLoading(true);
+
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemonNome}`;
+
     fetch(url)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Pokemon não encontrado'); 
+      }
+      return response.json()
+    })
     .then(json => {
-      console.log(json)
-      setPokemon(json)
+      setPokemon(json);
+      setLoading(false);
   })
     .catch(err => console.log(err));
   }
 
   useEffect(() => {
-    loadAPI();
+
+    loadAPI(input);
+
   }, []);
+
+  function handleInputChange(e) {
+    setInput(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    loadAPI(input);
+  }
+
   return (
     <div className="container">
       <header>
@@ -26,13 +48,27 @@ function App() {
         </strong>
       </header>
 
-      <div>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
-        <div>Name: {pokemon.name}</div>
-        <div>Nº {pokemon.id}</div>
-        <div>Peso: {pokemon.weight / 10} Kg</div>
-        <div>Altura: {pokemon.height / 10} m</div>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Digite o nome do Pokemon"
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      {loading && <div>Carregando...</div>}
+
+      {pokemon && !loading && (
+        <div>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
+          <div>Name: {pokemon.name}</div>
+          <div>Nº {pokemon.id}</div>
+          <div>Peso: {pokemon.weight / 10} Kg</div>
+          <div>Altura: {pokemon.height / 10} m</div>
+        </div>
+      )}
       
     </div>
   );
