28b54997b73101efe3943ab009fef1e712685ca4
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-03-26 21:30:43 -0300

    Corrigindo a constante pokemon e criando o style.css do projeto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, {useEffect, useState} from "react";
+import './style.css';
 
 function App() {
-  const {pokemon, setPokemon} = useState([]);
+  const [pokemon, setPokemon] = useState([]);
 
   function loadAPI() {
     let url = 'https://pokeapi.co/api/v2/pokemon/ditto';
     fetch(url)
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+      console.log(json)
+      setPokemon(json)
+  })
     .catch(err => console.log(err));
   }
 
@@ -15,8 +19,14 @@ function App() {
     loadAPI();
   }, []);
   return (
-    <div>
-      <h1>TESTE</h1>
+    <div className="container">
+      <header>
+        <strong>
+          Pokemon API
+        </strong>
+      </header>
+
+      
     </div>
   );
 }
